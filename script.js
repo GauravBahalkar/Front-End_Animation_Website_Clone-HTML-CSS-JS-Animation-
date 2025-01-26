cec7faef93b8aab15f403eebea7f36522c96a364
Gaurav Bahalkar <88779254+GauravBahalkar@users.noreply.github.com>
2025-01-26 19:15:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Swiper
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+   
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
